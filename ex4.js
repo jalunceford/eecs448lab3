a4553aa3fab0f7861f6890c07b31f70f2a522466
Jack Lunceford <jacklunceford@ku.edu>
2020-03-29 16:46:54 -0500

    Fixed border width issue

--- ex4.js
+++ ex4.js
@@ -1,16 +1,17 @@
 document.addEventListener("DOMContentLoaded", function(){
 	document.getElementById("btn").addEventListener("click", () => {
-		let red = document.getElementById("red").value;
-		let green = document.getElementById("green").value;
-		let blue = document.getElementById("blue").value;
-		let width = document.getElementById("width").value;
-		let red2 = document.getElementById("red2").value;
-		let green2 = document.getElementById("green2").value;
-		let blue2 = document.getElementById("blue2").value;
-		let p = document.getElementById("p");
-		p.style.borderWidth = width;
-		p.style.backgroundColor = 'rgb(' + red2 + ',' + green2 + ',' + blue2 + ')';
-		p.style.border = 'solid rgb(' + red + ',' + green + ',' + blue + ')';
+		let red = Number(document.getElementById("red").value);
+		let green = Number(document.getElementById("green").value);
+		let blue = Number(document.getElementById("blue").value);
+		let width = String(document.getElementById("width").value);
+		let red2 = Number(document.getElementById("red2").value);
+		let green2 = Number(document.getElementById("green2").value);
+		let blue2 = Number(document.getElementById("blue2").value);
+		
+		document.getElementById("p").style.borderWidth = width;
+		document.getElementById("p").style.backgroundColor = 'rgb(' + red2 + ',' + green2 + ',' + blue2 + ')';
+		document.getElementById("p").style.borderColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
+		'rgb(' + a + ',' + b + ',' + c + ')'
 		
 	});
 });
